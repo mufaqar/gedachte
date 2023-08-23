d917d7e95f05b9f4c06d089d8227d21b14402a0a
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-23 20:33:09 +0500

    mbl header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,49 +7,52 @@ import { GrClose } from 'react-icons/gr'
 function Header() {
     const [open, setOpen] = useState(false)
     return (
-        <header className='py-5 relative z-50'>
-            <div className='container mx-auto px-4 flex justify-between items-center'>
-                <div className='md:w-1/4 w-1/2'>
-                    <Link href="/">
-                        <Image src="/images/logo.png" alt='logo' width={230} height={65} className='' />
-                    </Link>
+        <header className=' relative z-50'>
+            <div className='container mx-auto flex md:flex-row flex-col-reverse gap-3 justify-between items-center'>
+                <div className='md:w-3/4 w-full py-3 md:px-0 px-4 flex justify-between items-center md:bg-transparent bg-sky-900'>
+                    <div className='md:w-1/3 w-1/2'>
+                        <Link href="/">
+                            <Image src="/images/logo.png" alt='logo' width={230} height={65} className='md:block hidden' />
+                            <Image src="/images/logo-white.png" alt='logo-white' width={141} height={40} className='md:hidden block' />
+                        </Link>
+                    </div>
+                    <nav className='md:w-2/3 w-1/2'>
+                        <button className='md:hidden block ml-auto text-white' onClick={() => setOpen(!open)}>
+                            {
+                                open ? (<GrClose size={26} color="#ffffff" />) : (<FaBars size={26} color="#ffffff" />)
+                            }
+                        </button>
+                        <ul className={`flex md:flex-row flex-col justify-center md:gap-10 md:static bg-transparent absolute ${open ? 'left-0 right-0 top-[9.5rem] gap-8 !bg-sky-900 rounded-tl-[20px] rounded-bl-[20px] px-6 py-8' : 'top-[-200px]'}`}>
+                            <li>
+                                <Link href="/" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
+                                    Diensten
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
+                                    Hulpmiddelen
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
+                                    Over Ons
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav className='md:w-2/4 w-1/2'>
-                    <button className='md:hidden block ml-auto' onClick={() => setOpen(!open)}>
-                        {
-                            open ? (<GrClose size={26} />) : (<FaBars size={26} />)
-                        }
-                    </button>
-                    <ul className={`flex md:flex-row flex-col justify-center md:gap-10 md:static bg-transparent absolute ${open ? 'left-0 right-0 top-20 gap-8 !bg-sky-900 rounded-tl-[20px] rounded-bl-[20px] px-6 py-8' : 'top-[-200px]'}`}>
-                        <li>
-                            <Link href="/" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
-                                Diensten
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
-                                Hulpmiddelen
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
-                                Over Ons
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#" className={`text-lg font-semibold text-black ${open ? "text-white" : ""}`}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <div className='md:w-1/4 md:block hidden'>
-                    <ul className='flex justify-end gap-10 items-center'>
+                <div className='md:w-1/4 w-full py-3 md:px-0 px-4'>
+                    <ul className='flex md:justify-end justify-center gap-10 items-center'>
                         <li>
                             <Link href="#" className='text-sm font-semibold uppercase bg-title_clr text-white py-4 px-6 rounded-[36px] block'>
                                 Plan Nu een Afspraak
