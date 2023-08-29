3b3d01731beb7c54ead89773f5244ccb1642788d
mufaqar <mufaqar@gmail.com>
2023-08-29 17:12:55 +0500

    FAQs

--- sanity/schema/faqs.jsx
+++ sanity/schema/faqs.jsx
@@ -1,62 +1,17 @@
 export default {
-     name: 'faqs',
-     type: 'document',
-     title: 'FAQS',
+     name: "faqs",
+     title: "Faqs",
+     type: "document",
      fields: [
-          {
-               name: 'question',
-               type: 'string',
-               title: 'Question'
-          },
-          {
-               title: 'Slug',
-               name: 'slug',
-               type: 'slug',
-               options: {
-                 source: 'question',
-                 maxLength: 200, // will be ignored if slugify is set
-                 slugify: input => input
-                                      .toLowerCase()
-                                      .replace(/\s+/g, '-')
-                                      .slice(0, 200)
-               }
-             },
-          {
-               name: 'createdby',
-               type: 'string',
-               title: 'Created By'
-          },
-          {
-               title: 'Category',
-               name: 'ctegory',
-               type: 'reference',
-               to: [{ type: 'categories' }]
-          },
-          {
-               name: 'answer',
-               type: 'array',
-               title: 'Answer',
-               of: [
-                    {
-                         type: 'block'
-                    },
-                    {
-                         type: 'image'
-                    }
-               ]
-          },
-          {
-               name: 'issue',
-               type: 'array',
-               title: 'Issue',
-               of: [
-                    {
-                         type: 'block'
-                    },
-                    {
-                         type: 'image'
-                    }
-               ]
-          }
+       {
+         title: "Question",
+         name: "title",
+         type: "text"
+       },
+       {
+         title: "Answer",
+         name: "answer",
+         type: "text"
+       }
      ]
-}
+   }
